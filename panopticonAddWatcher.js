bcc3e5565315e0e14a46249d2412dacde5facbdf
gregorysobotka <gregorysobotka@gmail.com>
2017-05-25 09:26:55 -0700

    Adding Initial work
    Worked locally, updating repo with current state of project.

---
+++ panopticonAddWatcher.js
@@ -0,0 +1,29 @@
+var MongoClient = require('mongodb').MongoClient;
+var app = new (require('express'))();
+var wt = require('webtask-tools');
+
+module.exports = function(ctx, callback){
+
+  var MLabUrl = ctx.data.MLabUrl;
+  var postBody = ctx.body;
+
+  if(typeof postBody === 'undefined' || !postBody.url || !postBody.statusCodes) 
+    return cb({"required": "POST:body:url, POST:body:statusCodes"})
+
+  var newWatcher = {
+    url: postBody.url,
+    statusCodes: postBody.statusCodes
+  };
+
+  MongoClient.connect(MLabUrl, function(err, db) {
+    
+    db
+      .collection('watching')
+      .insertOne(newWatcher);
+
+    cb(null, newWatcher);
+
+  });
+
+}
+
---
+++ panopticonUI.js
@@ -0,0 +1,104 @@
+var app = new (require('express'))();
+var wt = require('webtask-tools');
+
+// URL for POST request to add watcher
+const WebTasksAddUrl = "https://wt-71c861dbca0d933900444e2beba407fe-0.run.webtask.io/panopticonAddWatcher";
+
+// URL for GET request to check all watchers
+const WebTasksStatusUrl = "https://wt-71c861dbca0d933900444e2beba407fe-0.run.webtask.io/panopticonStatus";
+
+/*
+    NOTE: UI is for demo purposes only. Do not build UI's like this.
+*/
+
+app.get('/', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.end(bodyHtml());
+});
+
+function bodyHtml(){
+    return `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+                <meta charset="utf-8">
+                <title>Panopticon - The all seeing eye of the web</title>
+                <style> 
+                    .SUCCESS{background-color:#dff0d8;}
+                    .FAILURE{background-color:#f2dede;}
+                </style>
+            </head>
+            <body>
+                <div class="container">
+                    <div class="col-md-10">
+                        <h2>Panopticon</h2>
+                        <p>Need to keep an eye on the status of a website or service? Enter the URL and the expected HTTP response codes for that url.</p>
+                        <p>Want to keep an eye on everything yourself? simply make a GET request to <a href="${WebTasksStatusUrl}" target="_blank">this url</a>.</p>
+                        <form id="addWatcher">
+                            <label>URL to watch</label>
+                            <input type="url" id="url" class="form-control" placeholder="URL"/>
+                            <label>Accetpable StatusCodes</label>
+                            <input type="text" id="statusCodes" class="form-control" placeholder="200,301,302"/>
+                        </form>
+                        <button id="submit" class="btn btn-success form-control">Submit</button>
+                        <status id="services"></status>
+                    </div>
+                </div>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+                <script>
+
+                    // URL for POST request to add watcher
+                    var WebTasksAddUrl = "${WebTasksAddUrl}";
+
+                    // URL for GET request to check all watchers
+                    var WebTasksStatusUrl = "${WebTasksStatusUrl}";
+
+                    $(document).ready(function(){
+
+                        $.get(WebTasksStatusUrl, function(res){
+                            $('#services').html("<table class='table'>"+
+                                res.status.map((service)=> 
+                                    "<tr class='"+service.status+"'><td>"+service.status+"</td> <td>"+service.url+"</td><td>"+service.statusCode+"</td><td>"+service.expected.toString()+"</td></tr>"
+                                )
+                                .reduce((a,b)=>a+b,'<tr><th>Status</th><th>Url</th><th>Status code</th><th>Acceptable statusCodes</th></tr>'))+"</table>";
+                        });
+
+                        $('#submit').click(function(){
+
+                            var statusCodes = $('#statusCodes')
+                                                .val()
+                                                .replace(/\s/g,'') // remove all whitespace
+                                                .split(",") // seperate string values by comma and convert to array  
+                                                .filter((val)=>Number.isInteger(parseInt(val))); // iterate over array values and only allow for numbers that can be converted to int
+
+                            var url = $('#url').val();
+                            
+                            if(statusCodes.length === 0 || url.indexOf('http') === -1) return;
+                            
+                            $.ajax({
+                                url: WebTasksAddUrl,
+                                method:"POST",
+                                data: JSON.stringify({
+                                    url: url,
+                                    statusCodes: statusCodes
+                                }),
+                                contentType: "application/json; charset=utf-8",
+                                dataType: "json"
+                            }).done(function() {
+                                window.
+                                // ... status update of some kind 
+
+                            });
+
+                        });              
+
+                    });
+
+                </script>
+            </body>
+        </html>
+    `;
+}
+
+module.exports = wt.fromExpress(app);
