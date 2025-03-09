9cbfb5170daf79b25b2dac543a82f06198303030
Gregory Sobotka <gregorysobotka@gmail.com>
2025-03-09 15:32:38 -0700

    Adding swagger doc

--- api/readme.md
+++ api/readme.md
@@ -4,3 +4,6 @@ nvm use
 npm i
 npm start dev
 ```
+
+### Swagger Documentation
+- https://swagger.io/docs/specification/v2_0/basic-structure/
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs");
+const YAML = require('yaml');
+const swaggerFile  = fs.readFileSync('./swagger.yaml', 'utf8');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.parse(swaggerFile);
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
