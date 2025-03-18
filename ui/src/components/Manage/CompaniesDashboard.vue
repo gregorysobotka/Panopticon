45f185f6b7ace508e10e8d86d5618ef0e166939c
Gregory Sobotka <gregorysobotka@gmail.com>
2025-03-17 18:13:05 -0700

    cleanup

--- ui/src/components/Manage/CompaniesDashboard.vue
+++ ui/src/components/Manage/CompaniesDashboard.vue
@@ -1,30 +1,38 @@
 <template>
-  <h1 class="text-h4">Companies</h1>
-  <v-table class="mt-10">
-    <thead>
-      <tr>
-        <th class="text-left">
-          Name
-        </th>
-        <th class="text-left">
-          Created
-        </th>
-        <th class="text-left">
-          Updated
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="company in companies"
-        :key="company.displayname"
-      >
-        <td><v-btn variant="flat" color="primary" :to="companyLink(company.id)">{{ company.displayname }}</v-btn></td>
-        <td>{{ company.createdAt }}</td>
-        <td>{{ company.updatedAt }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+  <v-row>
+    <v-col cols="12">
+      <h1 class="text-h4">Companies</h1>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12">
+      <v-table class="mt-10">
+        <thead>
+          <tr>
+            <th class="text-left">
+              Name
+            </th>
+            <th class="text-left">
+              Created
+            </th>
+            <th class="text-left">
+              Updated
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="company in companies"
+            :key="company.displayname"
+          >
+            <td><v-btn variant="flat" color="primary" :to="companyLink(company.id)">{{ company.displayname }}</v-btn></td>
+            <td>{{ company.createdAt }}</td>
+            <td>{{ company.updatedAt }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
--- ui/src/components/Manage/CompanyDashboard.vue
+++ ui/src/components/Manage/CompanyDashboard.vue
@@ -1,30 +1,43 @@
 <template>
-  <h1 class="text-h4">{{companyName}}</h1>
-  <v-table class="mt-10">
-    <thead>
-      <tr>
-        <th class="text-left">Name</th>
-        <th class="text-left">Location</th>
-        <th class="text-left">Env</th>
-        <th class="text-left">Language</th>
-        <th class="text-left">Created</th>
-        <th class="text-left">Updated</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="site in sites"
-        :key="site.displayname"
-      >
-        <td><v-btn variant="flat" color="primary" :to="siteLink(companyID, site.id)">{{ site.displayname }}</v-btn></td>
-        <td>{{ site.location }}</td>
-        <td>{{ site.environment }}</td>
-        <td>{{ site.language }}</td>
-        <td>{{ site.createdAt }}</td>
-        <td>{{ site.updatedAt }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+  <v-row>
+    <v-col cols="10">
+      <h1 class="text-h4">
+        {{companyname}}
+      </h1>
+    </v-col>
+    <v-col cols="2">
+      <v-btn prepend-icon="fa:fa-plus" variant="outlined">
+        Button
+      </v-btn>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-table class="mt-10">
+      <thead>
+        <tr>
+          <th class="text-left">Name</th>
+          <th class="text-left">Location</th>
+          <th class="text-left">Env</th>
+          <th class="text-left">Language</th>
+          <th class="text-left">Created</th>
+          <th class="text-left">Updated</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="site in sites"
+          :key="site.displayname"
+        >
+          <td><v-btn variant="flat" color="primary" :to="siteLink(companyID, site.id)">{{ site.displayname }}</v-btn></td>
+          <td>{{ site.location }}</td>
+          <td>{{ site.environment }}</td>
+          <td>{{ site.language }}</td>
+          <td>{{ site.createdAt }}</td>
+          <td>{{ site.updatedAt }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-row>
   <Debug :dc="sites" />
 </template>
 
@@ -50,7 +63,7 @@
           }
           const json = await response.json();
           this.sites = json.sites;
-          this.companyName = json.displayname;
+          this.companyname = json.displayname;
 
         } catch (error) {
           console.error(error.message);
@@ -60,7 +73,7 @@
     },
     computed: {},
     data: () => ({
-      companyName: '',
+      companyname: '',
       sites: []
     }),
   }
